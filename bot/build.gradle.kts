6165cd169019062d572cc683b4e37d33d9be3099
lfonta2001 <lfontaine2001@gmail.com>
2026-07-02 01:41:26 -0300

    Fix for logback dependency and gradle update

--- bot/build.gradle.kts
+++ bot/build.gradle.kts
@@ -9,12 +9,13 @@ application {
 dependencies {
 
     implementation("net.dv8tion:JDA:6.3.2")
-
     implementation("ch.qos.logback:logback-classic:1.5.34")
-
     implementation("io.github.cdimascio:dotenv-java:3.2.0")
-
     implementation("org.reflections:reflections:0.10.2")
 
-    implementation("org.projectlombok:lombok:1.18.46")
+    compileOnly("org.projectlombok:lombok:1.18.46")
+    annotationProcessor("org.projectlombok:lombok:1.18.46")
+
+    testCompileOnly("org.projectlombok:lombok:1.18.46")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.46")
 }
